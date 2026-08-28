ad4790ed6a892e298ec66efc81975a3f306673e8
Nishtha Jaiswal <magic222nish@gmail.com>
2026-08-28 11:15:12 +0000

    Complete Question 3: Add basicIO integration

--- MT26212_build.sh
+++ MT26212_build.sh
@@ -1,7 +1,17 @@
+
+
 #!/bin/bash
 # RollNo: MT26212
 # Name: Nishtha Jaiswal
 
+# Compile the assembly system calls
 nasm -f elf64 syscall.S -o syscall.o
+
+# Compile the supplied I/O class
+g++ -c -nostdlib -fno-exceptions basicIO.cpp -o basicIO.o
+
+# Compile your main program
 g++ -c -nostdlib -fno-exceptions MT26212_main.cpp -o MT26212_main.o
-ld syscall.o MT26212_main.o -o myprogram
+
+# Link everything together into the final executable
+ld syscall.o basicIO.o MT26212_main.o -o myprogram
--- MT26212_main.cpp
+++ MT26212_main.cpp
@@ -1,6 +1,41 @@
 // RollNo: MT26212
 // Name: Nishtha Jaiswal
 
+//int main() {
+//   return 0;
+//}
+// RollNo: MT26212
+// Name: Nishtha Jaiswal
+
+#include "basicIO.h"
+
 int main() {
+    io.activateInput(); 
+
+    char name[256];
+    io.outputstring("Enter your name: ");
+    io.inputstring(name, 256);
+
+    io.outputstring("Enter your age: ");
+    int age = io.inputint();
+
+    // basic bounds check for age
+    if (age <= 0 || age > 120) {
+        io.errorstring("Invalid age entered.\n");
+        io.terminate();
+        return 1; 
+    }
+
+    char confirm[5];
+    io.outputstring("Is this correct? (y/n): ");
+    io.inputstring(confirm, 5);
+
+    if (confirm[0] == 'y' || confirm[0] == 'Y') {
+        io.outputstring("Information confirmed.\n");
+    } else {
+        io.outputstring("Information discarded.\n");
+    }
+
+    io.terminate();
     return 0;
 }
